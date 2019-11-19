a6795ec3dc0cd10614220d168cd6307eb9d63658
hello@bennyschmidt.com <hello@bennyschmidt.com>
2019-11-19 11:32:28 -0800

    featured article

--- actions/Blog.js
+++ actions/Blog.js
@@ -26,8 +26,7 @@ class Blog extends ƒ.Action {
         publishDate: 'Aug 24 2019 21:02 PDT',
         title: 'Like React for Front-End? Try Fire For Backend',
         body: [
-          'Fire is a library for backend JavaScript.',
-          'Fire makes it painless to create application backends. Design simple Actions for each route in your application, and Fire will efficiently merge your updates with the global application state and save just the right data to the database, retaining a single-source of truth at the application level. Declarative APIs make your requests and responses more predictable, simpler to understand, and easier to debug.',
+          'Fire is a library for backend JavaScript that makes it painless to create application backends. Design simple Actions for each route in your application, and Fire will efficiently merge your updates with the global application state and save just the right data to the database, retaining a single-source of truth at the application level. Declarative APIs make your requests and responses more predictable, simpler to understand, and easier to debug.',
           'Build encapsulated Actions that manage their own state, then compose them to make complex APIs. Because the application state can be derived from the state of all Actions, there is no need to create a database schema, or set up an initial database – Fire generates the database, and maintains the state of it as Actions are fired.',
           'Fire doesn\'t make assumptions about your application\'s front-end, so you can develop new features in Fire with your existing front-end code. Fire can also be used along-side many other Fire Nodes in a service network, sharing state information while retaining a single source of truth.'
         ],
@@ -80,8 +79,7 @@ class Blog extends ƒ.Action {
           publishDate: 'Aug 24 2019 21:02 PDT',
           title: 'Like React for Front-End? Try Fire For Backend',
           body: [
-            'Fire is a library for backend JavaScript.',
-            'Fire makes it painless to create application backends. Design simple Actions for each route in your application, and Fire will efficiently merge your updates with the global application state and save just the right data to the database, retaining a single-source of truth at the application level. Declarative APIs make your requests and responses more predictable, simpler to understand, and easier to debug.',
+            'Fire is a library for backend JavaScript that makes it painless to create application backends. Design simple Actions for each route in your application, and Fire will efficiently merge your updates with the global application state and save just the right data to the database, retaining a single-source of truth at the application level. Declarative APIs make your requests and responses more predictable, simpler to understand, and easier to debug.',
             'Build encapsulated Actions that manage their own state, then compose them to make complex APIs. Because the application state can be derived from the state of all Actions, there is no need to create a database schema, or set up an initial database – Fire generates the database, and maintains the state of it as Actions are fired.',
             'Fire doesn\'t make assumptions about your application\'s front-end, so you can develop new features in Fire with your existing front-end code. Fire can also be used along-side many other Fire Nodes in a service network, sharing state information while retaining a single source of truth.'
           ],

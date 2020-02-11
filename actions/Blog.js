f734fe65194595965a9c2d111bb925a8cf78bf22
hello@bennyschmidt.com <hello@bennyschmidt.com>
2020-02-10 17:13:30 -0800

    update articles

--- actions/Blog.js
+++ actions/Blog.js
@@ -23,18 +23,18 @@ class Blog extends ƒ.Action {
     this.setState({
       route: '/blog',
       article: {
-        publishDate: 'Jan 11 2020 19:47 PDT',
-        title: 'Street - A Skateboarding MMORPG',
+        publishDate: 'Feb 10 2020 17:00',
+        title: 'An easy-to-theme React input with built-in analytics',
         body: [
-          'I built this prototype - "Street" - over the last few days to explore this skateboard MMO idea further.',
-          'The concept is to have a more realistic and challenging skate game, where the player has to master each trick, all while playing online with others around the world.',
-          '<iframe width="560" height="315" src="https://www.youtube.com/embed/nU02omhKUt8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+          'SmartInput is the best text input element for React because it\'s so easy to use and style, and for advanced users it provides rich analytics about end usage.',
+          'Theming with JSX attributes is super simple: <code><SmartInput theme="Material" /></code>, for example, restyles the input to the "Material" theme (defined in themes.css). You can also override styles or CSS classes like you would with any React component.',
+          'SmartInput captures and reveals rich insights and analytics in real-time, and maintains a detailed history of what\'s been entered and submitted in the past. Other details like time spent filling out a form, typing speed, keystrokes, backspaces, and more are also exposed. You can also configure a SmartInput to report keyword entries or emails entered to a specified API endpoint - like your favorite analytics software, or your application backend. Use <code><SmartInput profile="Standard" /></code> to track according to the <code>StandardProfile</code> (defined in <code>/profiles</code>).'
         ],
-        url: 'https://www.youtube.com/watch?v=nU02omhKUt8'
+        url: 'https://github.com/exactchange/smart-input'
       },
       articles: [
         {
-          publishDate: 'May 20 2019 15:21 PDT',
+          publishDate: 'May 20 2019 15:21',
           title: 'From React Native to the App Store',
           body: [
             'Daily Tarot for iPhone',
@@ -45,17 +45,7 @@ class Blog extends ƒ.Action {
           url: 'https://github.com/exactchange/daily-tarot'
         },
         {
-          publishDate: 'July 4 2019 04:20 PDT',
-          title: 'Working on This Battle Royale Game...',
-          body: [
-            'Blacklisted is a Battle Royale game I started building last year. I recently picked the project back up, and will hopefully have some exciting developments to share by 2020!',
-            'Don\'t want to wait for launch? You can join <a href="https://playblacklisted.com" target="_blank" style="text-decoration:underline">early access</a> and test alpha gameplay right now!',
-            '<iframe width="560" height="315" src="https://www.youtube.com/embed/8_kq3_0YwIg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-          ],
-          url: 'https://www.youtube.com/watch?v=8_kq3_0YwIg'
-        },
-        {
-          publishDate: 'Aug 24 2019 21:02 PDT',
+          publishDate: 'Aug 24 2019 21:02',
           title: 'Like React for Front-End? Try Fire For Backend',
           body: [
             'Fire is a library for backend JavaScript that makes it painless to create application backends. Design simple Actions for each route in your application, and Fire will efficiently merge your updates with the global application state and save just the right data to the database, retaining a single-source of truth at the application level. Declarative APIs make your requests and responses more predictable, simpler to understand, and easier to debug.'
@@ -63,24 +53,14 @@ class Blog extends ƒ.Action {
           url: 'https://github.com/exactchange/fire'
         },
         {
-          publishDate: 'Jan 9 2020 08:55 PDT',
-          title: 'A new MMO?',
+          publishDate: 'Feb 10 2020 17:00',
+          title: 'An easy-to-theme React input with built-in analytics',
           body: [
-            'A lot of skateboard games coming out this year, but few if any are fully online. Imagine a Tony Hawk\'s Pro Skater in a persistent online world &ndash; that\'s the vision!',
-            'Some of the functionality from my last project is being migrated over and perfected in this game (authentication, physics engine, real-time persistence, player queues, chat, etc.). Excited to share gameplay content soon!',
-            '<iframe width="560" height="315" src="https://www.youtube.com/embed/Rls5Hy90J1Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+            'SmartInput is the best text input element for React because it\'s so easy to use and style, and for advanced users it provides rich analytics about end usage.',
+            'Theming with JSX attributes is super simple: <code><SmartInput theme="Material" /></code>, for example, restyles the input to the "Material" theme (defined in themes.css). You can also override styles or CSS classes like you would with any React component.',
+            'SmartInput captures and reveals rich insights and analytics in real-time, and maintains a detailed history of what\'s been entered and submitted in the past. Other details like time spent filling out a form, typing speed, keystrokes, backspaces, and more are also exposed. You can also configure a SmartInput to report keyword entries or emails entered to a specified API endpoint - like your favorite analytics software, or your application backend. Use <code><SmartInput profile="Standard" /></code> to track according to the <code>StandardProfile</code> (defined in <code>/profiles</code>).'
           ],
-          url: 'https://www.youtube.com/watch?v=Rls5Hy90J1Y'
-        },
-        {
-          publishDate: 'Jan 11 2020 19:47 PDT',
-          title: 'Street - A Skateboarding MMORPG',
-          body: [
-            'I built this prototype - "Street" - over the last few days to explore this skateboard MMO idea further.',
-            'The concept is to have a more realistic and challenging skate game, where the player has to master each trick, all while playing online with others around the world.',
-            '<iframe width="560" height="315" src="https://www.youtube.com/embed/nU02omhKUt8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-          ],
-          url: 'https://www.youtube.com/watch?v=nU02omhKUt8'
+          url: 'https://github.com/exactchange/smart-input'
         }
       ],
       requestCount: 0
